7e4e4b7f0dc54f22de287354cc58dafc903bba4a
Jeremy Stein <saplyng@gmail.com>
2018-08-28 11:16:15 -0500

    forgot to save, so i saved

--- Java/lesson2.java
+++ Java/lesson2.java
@@ -1,30 +1,89 @@
 public class main {
   public static void main(String[] args){
-    int iCounter = 0;
-    double dPi = 3.14;
-    boolean bValid = true;
-    char cInitial = 'c'; //single quote for characters not double quote
-    String sName = "jeremy"; //String is capitalized and used double quotes
+  //   int iCounter = 0;
+  //   double dPi = 3.14;
+  //   boolean bValid = true;
+  //   char cInitial = 'c'; //single quote for characters not double quote
+  //   String sName = "jeremy"; //String is capitalized and used double quotes
+  //
+  //   double ans = 0
+  //
+  // //  ans = Double.parseDouble(STRING) / 5.00
+  //
+  //   iCounter = iCounter + 1;
+  //   iCounter++;
+  //   ++iCounter;
+  //
+  //   iCounter = iCounter - 1;
+  //   iCounter--;
+  //   --iCounter;
+  //
+  //   ans = Math.
+  //   //AND &&
+  //   //OR ||
+  //   //NOT !
+  //   //EQUALS TO ==
+  //   if(bValid && 4>5){
+  //     //true - run this code
+  //
+  //   }else if (iCouter == 0){
+  //     //true - run this code
+  //   }else {
+  //
+  //   }
+  //
+  //   switch(iCounter){
+  //     case 1: {
+  //       // run all code
+  //       break;
+  //     }case 2: {
+  //       //run all code
+  //       break;
+  //     }
+  //     default:{
+  //       //no solution above met this code
+  //       break;
+  //     }
+  //
+  //   }
+  //
+  //   switch (sName){
+  //     case "Dave":{
+  //       break;
+  //     }
+  //     case "Mike":{
+  //       break;
+  //     }
+  //     default:{
+  //       break;
+  //     }
+  //   }
+  //
+  //
+  //   System.out.println("Answer is " + ans);
+  //
+  //
+  //   int iCount = 0;
+  //
+  //   while(iCount < 5){
+  //     //run any code here
+  //     iCount++;
+  //   }
+  //
+  //   iCount = 0;
+  //   do{
+  //     iCount++;
+  //   }while(iCount > 5);
+  //
+  //   for(int i = 0; i < 5; i++){
+  // //
+  //   }
+string sName = "";
 
-    double ans = 0
+  Scanner sc new Scanner(System.in);
+  System.out.print("Enter your name: ");
+  sName = sv.nextLine();
 
-  //  ans = Double.parseDouble(STRING) / 5.00
-
-
-    //AND &&
-    //OR ||
-    //NOT !
-    //EQUALS TO ==
-    if(bValid && 4>5){
-      //true - run this code
-
-    }else if (iCouter == 0){
-      //true - run this code
-    }else {
-      
-    }
-
-    System.out.println("Answer is " + ans);
-
+  system.out.println("your name is " + sName);
   }
 }
