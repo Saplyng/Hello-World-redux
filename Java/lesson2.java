fcde94a9bae11cf0b4363e6e85ea03da55543161
Jeremy Stein <saplyng@gmail.com>
2018-08-28 11:13:59 -0500

    java lesson we had in class, all about operators and basic variables

---
+++ Java/lesson2.java
@@ -0,0 +1,30 @@
+public class main {
+  public static void main(String[] args){
+    int iCounter = 0;
+    double dPi = 3.14;
+    boolean bValid = true;
+    char cInitial = 'c'; //single quote for characters not double quote
+    String sName = "jeremy"; //String is capitalized and used double quotes
+
+    double ans = 0
+
+  //  ans = Double.parseDouble(STRING) / 5.00
+
+
+    //AND &&
+    //OR ||
+    //NOT !
+    //EQUALS TO ==
+    if(bValid && 4>5){
+      //true - run this code
+
+    }else if (iCouter == 0){
+      //true - run this code
+    }else {
+      
+    }
+
+    System.out.println("Answer is " + ans);
+
+  }
+}
