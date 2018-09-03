d9a00c3f3b45afb2ccc7cc5d6c4edc76b479ee62
Jeremy Stein <saplyng@gmail.com>
2018-09-03 15:50:51 -0500

    finished kotlin Operators file

---
+++ Kotlin/Operators.kt
@@ -0,0 +1,13 @@
+fun main(args: Array<String>) {
+  var a = 42
+  var b = 3
+  var c = 56
+
+  c-- // c == 55
+  c++ // c == 56
+  b += 7 // b == 10
+  a - b // = 32
+  a % b // = 0
+  a == b // false
+  a < b || b < c // true
+}
