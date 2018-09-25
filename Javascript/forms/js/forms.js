ababb4f636b7cda8ba1315132a52906dab5b7e4c
Jeremy Stein <saplyng@gmail.com>
2018-09-24 21:30:44 -0500

    stuff?

--- Javascript/forms/js/forms.js
+++ Javascript/forms/js/forms.js
@@ -1,10 +1,12 @@
 // get id from html and use them
 var $ = function (id) {
+  "use strict"
     return document.getElementById(id);
 }
 
 
 var calculate_cost = function () {
+  "use strict"
     // we have to parse - or change - the values to floats. Form values are strings by default.
     var length = parseFloat($("frontlength").value);
     var width = parseFloat($("frontwidth").value);
@@ -14,6 +16,7 @@ var calculate_cost = function () {
     var sidewidth = parseFloat($("sidewidth").value);
     var sidelength2 = parseFloat($("sidelength2").value);
     var sidewidth2 = parseFloat($("sidewidth2").value);
+    console.log(sidewidth2);
 
     if (isNaN(length) || isNaN(width) | isNaN(backlength) | isNaN(backwidth) | isNaN(sidelength) | isNaN(sidewidth) | isNaN(sidelength2) | isNaN(sidewidth2)){
         alert("You have an invalid value. Correct and click submit")
@@ -22,9 +25,12 @@ var calculate_cost = function () {
         var total = ((length * width) + (backlength * backwidth) + (sidelength * sidewidth) + (sidelength2 * sidewidth2));//50 cents per square foot
         total = total.toFixed(2);
         $("paintingcost").value = "$ " + total;
+        console.log(total);
     }
 }
 window.onload = function () {
+  "use strict"
     $("submit").onclick = calculate_cost;
-
+    console.log(calculate_cost);
+    
 }
