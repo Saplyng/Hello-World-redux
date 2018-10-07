9d2c49430654d522750ee6e9b0d701c4a3505990
Jeremy Stein <saplyng@gmail.com>
2018-10-06 21:08:17 -0500

    yep yep

---
+++ Swift/playground-files/M7_Exercise_2_Protocols.playground/M7_Exercise_2_Protocols.playground/Contents.swift
@@ -0,0 +1,15 @@
+/*:
+ ## Module 7 Exercise 2 -- Protocols
+ *For each of the numbered steps in the comments, add the code requested on the line(s) below the instruction.*
+ 1. Create a *structure* that defines a Point having Double x and y values.
+ */
+
+
+//: 2. Create a Circle *class* that includes a Point object for its center and a Double value for its radius. The center point should default to (0.0, 0.0) and the radius should always have a positive value.
+
+
+//: 3. Create an *extension* to the Circle class that implements the == function defined in the Equatable *protocol*. Depending upon your implementation, you may need to make an adjustment to the Point struct for protocol compliance. Equatable protocol documentation can be found [here](https://developer.apple.com/documentation/swift/equatable?changes=_3).
+
+
+//: 4. Create two Circle objects and test them for equality using the == operator.
+
