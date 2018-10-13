3362c966785db8bfc69af7d20a321dc7946c2873
Jeremy Stein <saplyng@gmail.com>
2018-10-13 11:05:45 -0500

    other stuff!

---
+++ Kotlin/inheiritence/customer.kt
@@ -0,0 +1,21 @@
+open class Customer (
+    customerName: String?,
+    customerPhone: String?,
+    customerAddress: String?,
+    squareFootage: Double?){}
+
+class Commercial (
+    customerName: String?,
+    customerPhone: String?,
+    customerAddress: String?,
+    squareFootage: Double?,
+    propertyName: String?,
+    squareFootage){
+
+    for (1000 in squareFootage){
+    commercialRate: Int?,
+    multiProperty: Int?) : Customer(customerName,
+                                        customerPhone,
+                                        customerAddress,
+        commercialRate = commercialRate + 50}
+}
