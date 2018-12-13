bf228cd41ded59bee806dbe751e89748aa60c914
Jeremy Stein <saplyng@gmail.com>
2018-12-12 19:46:48 -0600

    testing some junk

---
+++ Javascript/functions/js/script.js
@@ -0,0 +1,19 @@
+function change01() {
+  document.getElementById("large").src = "img/okami1.jpg";
+}
+
+function change02() {
+  document.getElementById("large").src = "img/okami2.jpg";
+}
+
+function change03() {
+  document.getElementById("large").src = "img/okami3.jpg";
+}
+
+function change04() {
+  document.getElementById("large").src = "img/okami9.jpg";
+}
+
+function change05() {
+  document.getElementById("large").src = "img/okami10.jpg";
+}
