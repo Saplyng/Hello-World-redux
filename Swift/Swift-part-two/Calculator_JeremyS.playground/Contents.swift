76e86b74197dd39352a025aca41e6c5d38c30a52
Jeremy Stein <saplyng@gmail.com>
2019-03-29 16:21:40 -0500

    calculator mcthingy

---
+++ Swift/Swift-part-two/Calculator_JeremyS.playground/Contents.swift
@@ -0,0 +1,23 @@
+import UIKit
+
+
+//our input variables
+let x = 100.00
+let y = 200.00
+
+
+let addition  = x + y
+let subtraction = x - y
+let multiplication = x * y
+let division = x / y
+let percentage = division * 100
+
+
+
+print("value x = \(x)")
+print("value y = \(y)")
+print("addition : \(addition)")
+print("subtraction : \(subtraction)")
+print("multiplication : \(multiplication)")
+print("division : \(division)")
+print("percentage : \(percentage)%")
