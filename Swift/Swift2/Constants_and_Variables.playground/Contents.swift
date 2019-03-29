4e1e361acbd6dcdae4068e0e0841565d3d1218f8
Jeremy Stein <saplyng@gmail.com>
2019-03-28 21:38:24 -0500

    constants and variables

---
+++ Swift/Swift2/Constants_and_Variables.playground/Contents.swift
@@ -0,0 +1,20 @@
+import UIKit
+
+var number1: Int = 32
+var number2: Float = 42.0
+var number3: Double = 22.00
+
+let number4: Int = 12
+let number5: Float = 60.0
+let number6: Double = 70.00
+
+let stringName: String = "I'm a string"
+let number7: Float = 7777.7
+let number8: Double = 7777.77777777777
+
+let badconstant = 55
+badconstant = 44
+
+let number9: Float = 4
+//the 4 turns into 4.0 because, floats
+
