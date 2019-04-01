63d562423c788b3f052298994efb60bbbe045f83
Jeremy Stein <saplyng@gmail.com>
2019-03-31 22:59:25 -0500

    added madlibs

---
+++ Swift/Swift-part-two/madlibs_JeremyS.playground/Contents.swift
@@ -0,0 +1,67 @@
+import UIKit
+
+print("Hello, User! welcome to madlibs!!! The massively fun,")
+print("pee your pants, mad libs game!")
+print("")
+
+
+print("")
+
+print("First things first to get started we\'re going")
+print("to need a few words to make our madlib.")
+print("So if you could provide the following")
+print("words we\'ll get on our way.")
+
+print("")
+
+// put all the words to replace in here
+// that will make it easy to do later and
+// I can do the other stuff now
+
+let name = "Doug"                      //A person's name
+let item = "desk"                      //A noun
+let animal = "cow"                    //an animal
+let item2 = "tranquility"                     //A concept
+let color = "indigo"                     //a color
+let place = "Mount Rushmore"                     // a building
+let website = "TheOnion.com"                   //a website name
+let technology = "Walkie Talkie"                //a piece of technology
+let animal2 = "walrus"                   // some other animal
+
+
+print(" ")
+print(" ")
+print(" ")
+
+print(name + " the " + color + " " + animal2)
+print("have you any " + item + "s?")
+
+print(" ")
+
+print("\"No sir, no sir, some nincompoop stole")
+print("all my " + item + "s,")
+print(technology + "s, and Gaming systems.\"")
+
+print(" ")
+
+print("Luckily, the " + animal)
+print("insurance agency recently")
+print("helped " + name + " with")
+print(item2 + " insurance")
+
+print(" ")
+
+print("Everything stolen was replaced")
+
+print(" ")
+
+print("And the hooligan")
+print("living in the " + place)
+print("was caught selling")
+print("the stolen goods on " + website)
+
+print(" ")
+print(" ")
+print(" ")
+
+
